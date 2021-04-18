34dbe1b2d79091a2453a2936934477918e7dea1d
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-04-18 16:01:00 +0200

    Moving .js files

--- navbar-mobile.js
+++
@@ -1,9 +0,0 @@
-        const hamburger = document.querySelector(".hamburger");
-        const navigation = document.querySelector(".navigation-list");
-        const lines = document.querySelectorAll(".line");
-        hamburger.addEventListener("click", () => {
-            navigation.classList.toggle("navigation-list-down");
-            lines.forEach(line => {
-                line.classList.toggle("active");
-            })
-        })
