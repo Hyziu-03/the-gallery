0411dc041b7d6861af7fb79e1076e9989007d625
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-04-25 14:11:51 +0200

    FIle formatting

--- js/links-with-offset.js
+++ js/links-with-offset.js
@@ -7,29 +7,7 @@ var notableDesigners = document.getElementById('notable-designers');
 var famousCryptographers = document.getElementById('famous-cryptographers');
 var headerOffset = document.getElementById('header').clientHeight * 1.5;
 
-offer.addEventListener('click', function() {
-    var categoriesSection = document.getElementById('categories-container');
-    var targetPosition = categoriesSection.getBoundingClientRect().top;
-    var scrollTarget = targetPosition - headerOffset;
-
-    window.scrollTo({
-        top: scrollTarget, 
-        behavior: 'smooth'
-    });
-});
-
-contact.addEventListener('click', function() {
-    var form = document.getElementById('form-container');
-    var targetPosition = form.getBoundingClientRect().top;
-    var scrollTarget = targetPosition - headerOffset;
-
-    window.scrollTo({
-       top: scrollTarget,
-       behavior: 'smooth' 
-    });
-});
-
-landingPage.addEventListener('click', function() {
+offer.addEventListener('click', function () {
     var categoriesSection = document.getElementById('categories-container');
     var targetPosition = categoriesSection.getBoundingClientRect().top;
     var scrollTarget = targetPosition - headerOffset;
@@ -40,18 +18,40 @@ landingPage.addEventListener('click', function() {
     });
 });
 
-notableProgrammers.addEventListener('click', function() {
+contact.addEventListener('click', function () {
+    var form = document.getElementById('form-container');
+    var targetPosition = form.getBoundingClientRect().top;
+    var scrollTarget = targetPosition - headerOffset;
+
+    window.scrollTo({
+        top: scrollTarget,
+        behavior: 'smooth'
+    });
+});
+
+landingPage.addEventListener('click', function () {
+    var categoriesSection = document.getElementById('categories-container');
+    var targetPosition = categoriesSection.getBoundingClientRect().top;
+    var scrollTarget = targetPosition - headerOffset;
+
+    window.scrollTo({
+        top: scrollTarget,
+        behavior: 'smooth'
+    });
+});
+
+notableProgrammers.addEventListener('click', function () {
     window.location.href = 'programmers/bill-gates';
 });
 
-socialMediaCreators.addEventListener('click', function() {
+socialMediaCreators.addEventListener('click', function () {
     window.location.href = 'social-media-creators/kevin-systrom';
 });
 
-notableDesigners.addEventListener('click', function() {
+notableDesigners.addEventListener('click', function () {
     window.location.href = 'designers/steve-jobs';
 });
 
-famousCryptographers.addEventListener('click', function() {
+famousCryptographers.addEventListener('click', function () {
     window.location.href = 'cryptographers/marian-rejewski';
 });
